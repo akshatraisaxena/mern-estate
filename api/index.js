b06d72b220456360946e633c96662abf11951fc4
aksahatraisaxena <akshatsaxena2302@gmail.com>
2024-09-14 10:47:19 +0530

    authentication route created

--- api/index.js
+++ api/index.js
@@ -1,17 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
+
 import dotenv from 'dotenv';
 dotenv.config();
+
+const app = express();
 
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("Connected to MongoDB");
 }).catch((err)=>{
     console.error(err);
 })
-const app = express();
+
+app.use(express.json());
 
 app.use('/api/user',userRouter);
+app.use('/api/auth', authRouter);
 
 app.listen(3000 , () => {
     console.log ("server is running");
