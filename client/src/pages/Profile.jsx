b0bd7d4cba0cca7c6c9ab7f08a15e00da8ee1ab6
aksahatraisaxena <akshatsaxena2302@gmail.com>
2024-09-21 15:32:36 +0530

    image upadate functionality and handling error also shows the upload percentage

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,12 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {useSelector} from 'react-redux'
+import { useRef, useEffect } from 'react'
+import {getDownloadURL, getStorage,  ref, uploadBytesResumable } from 'firebase/storage'
+import { app } from '../firebase'
+
+
 const Profile = () => {
+  const fileRef = useRef(null)
   const {currentUser} = useSelector((state)=>state.user)
+  const [file, setfile] = useState(undefined)
+  const [fileper, setFileper] = useState(0)
+  const [fileUploadError, setfileUploadError] = useState(false)
+  const [formData, setformData] = useState({})
+  
+  useEffect(()=>{
+    if(file){
+      handleFileUpload(file);
+    }
+  },[file]);
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    //  passed app from firebase that we have exported
+
+    const fileName=new Date().getTime() + file.name;
+    // this will help to give a unique file name so if you added same file twice it will prevent the app from error
+
+    const storageRef= ref(storage,fileName);
+    const uploadTask= uploadBytesResumable(storageRef,file)
+    // to see the percentage of file uploading , and also to track the error we can use this uploadTask
+
+    uploadTask.on('state_changed', (snapshot) => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      setFileper(Math.round(progress))
+    },
+    (error)=>{
+      setfileUploadError(true)
+    },
+    ()=>{
+      getDownloadURL(uploadTask.snapshot.ref).then
+     ( (downloadUrl)=>{
+        setformData({...formData, avatar: downloadUrl})
+      })
+    }
+  );
+  }
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-2xl font-semibold text-center my-5'>Profile</h1>
       <form className='flex flex-col gap-4'>
-        <img className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-1' src={currentUser.avatar} alt="profile" />
+        <input onChange={(e) => setfile(e.target.files[0])} type="file" ref={fileRef} hidden accept='image/*' />
+        <img onClick={()=>fileRef.current.click()} className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-1' 
+         src={formData.avatar || currentUser.avatar} alt="profile" />
+        <p className='text-sm self-center'>
+         {fileUploadError ? 
+          (<span className='text-red-700'>Error image upload(image must be less then 5mb)</span>):
+           fileper>0 && fileper<100 ?
+           (<span className='text-slate-700'>{`Uploading ${fileper}%`}</span>):
+            fileper === 100 ? 
+           (<span className='text-green-700'>Image uploaded successfully</span>)
+           :
+           ""
+         }
+        </p>
         <input type="text" placeholder='username' id='username' className='border p-3 rounded-lg ' />
         <input type="email" placeholder='email' id='email' className='border p-3 rounded-lg ' />
         <input type="password" placeholder='password' id='password' className='border p-3 rounded-lg ' />
